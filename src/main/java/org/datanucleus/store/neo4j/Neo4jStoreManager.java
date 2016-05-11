5c7928cf32fe7c65df5b486aea1ff1b3728148b5
andyjefferson <andy@datanucleus.org>
2016-05-11 18:02:36 +0100

    Supported query options

--- src/main/java/org/datanucleus/store/neo4j/Neo4jStoreManager.java
+++ src/main/java/org/datanucleus/store/neo4j/Neo4jStoreManager.java
@@ -92,6 +92,8 @@ public class Neo4jStoreManager extends AbstractStoreManager
         set.add(StoreManager.OPTION_ORM);
         set.add(StoreManager.OPTION_ORM_EMBEDDED_PC);
         set.add(StoreManager.OPTION_TXN_ISOLATION_READ_COMMITTED);
+        set.add(StoreManager.OPTION_QUERY_JDOQL_BULK_DELETE);
+        set.add(StoreManager.OPTION_QUERY_JPQL_BULK_DELETE);
         return set;
     }
 
