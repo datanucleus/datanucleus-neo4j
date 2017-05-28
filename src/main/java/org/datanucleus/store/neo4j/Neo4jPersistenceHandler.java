79361d6f4319a2d949b2f7d634628e0642a37be2
andyjefferson <andy@datanucleus.org>
2017-05-28 15:32:55 +0100

    Support core-224

--- src/main/java/org/datanucleus/store/neo4j/Neo4jPersistenceHandler.java
+++ src/main/java/org/datanucleus/store/neo4j/Neo4jPersistenceHandler.java
@@ -423,7 +423,7 @@ public class Neo4jPersistenceHandler extends AbstractPersistenceHandler
                 // Version object so calculate version to store with
                 Object currentVersion = op.getTransactionalVersion();
                 VersionMetaData vermd = cmd.getVersionMetaDataForClass();
-                Object nextVersion = ec.getNextVersion(vermd, currentVersion);
+                Object nextVersion = ec.getLockManager().getNextVersion(vermd, currentVersion);
                 op.setTransactionalVersion(nextVersion);
 
                 if (vermd.getFieldName() != null)
